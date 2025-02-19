4207e6b0856790292da0629187ef3764de092d82
Juanmita <juanmapimientae@gmail.com>
2025-02-19 08:02:49 -0500

    Created database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataProviders } from './config/database.providers';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,],
+  providers: [AppService, ...dataProviders],
+  exports: [...dataProviders]
 })
 export class AppModule {}
