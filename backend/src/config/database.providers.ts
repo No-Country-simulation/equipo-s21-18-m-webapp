0c61be2b2b2c8d2317053de0cc7fc2632b6f2bcd
Juanmita <juanmapimientae@gmail.com>
2025-02-19 10:21:37 -0500

    Resolved connection with database

--- backend/src/config/database.providers.ts
+++ backend/src/config/database.providers.ts
@@ -4,6 +4,6 @@ export const dataProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect(`'${process.env.DATABASE_STRING}'`),
+            mongoose.connect('mongodb+srv://juanmapimientae:nocountrysonlomax123@fitloverdatabase.6eswm.mongodb.net/?retryWrites=true&w=majority&appName=FitLoverDatabase'),
     },
 ];
