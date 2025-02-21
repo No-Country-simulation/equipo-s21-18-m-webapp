e4deb3378d4d443b413670155f6e1a381e372bfd
Gaston095 <Gastonsalas095@gmail.com>
2025-02-21 18:15:59 -0300

    Se crea estructura basica de rutas con react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  
   return (
     <>
-      <h1 className="text-4xl text-amber-900">Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
