5fae35a9e50a2002bbefb79c989efc45d136eaa8
Gaston095 <Gastonsalas095@gmail.com>
2025-02-26 13:48:49 -0300

    Se agregan formulario de registro con validaciones

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,16 @@
 export default function Login() {
     return (
         <>
-            <h1>Login</h1>
+            <section>
+                <h1>Welcome back</h1>
+                <p>Enter your credentials to access your account</p>
+                <form>
+                    <span>Email</span>
+                    <input />
+                    <span>Password</span>
+                    <input />
+                </form>
+            </section>
         </>
     )
 }
