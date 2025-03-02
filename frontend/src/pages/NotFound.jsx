3f10a44d5485526d02809763ba110066960236b3
Gaston095 <Gastonsalas095@gmail.com>
2025-03-02 19:47:29 -0300

    Se agrega componente NotFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-    return (
-        <>
-            <h1>404 Not Found</h1>
-        </>
-    )
+  return (
+    <>
+      <body className="flex h-screen flex-col items-center justify-center bg-gray-100">
+        <div className="flex flex-col items-center">
+          <h1 className="text-[120px] font-extrabold text-gray-700">404</h1>
+          <p className="mb-6 text-2xl font-medium text-gray-600">
+            Page Not Found
+          </p>
+          <Link
+            to="/"
+            className="rounded-md bg-primary px-4 py-2 font-medium text-white transition-all duration-200 ease-in-out hover:bg-indigo-600"
+          >
+            Go Home
+          </Link>
+        </div>
+      </body>
+    </>
+  );
 }
