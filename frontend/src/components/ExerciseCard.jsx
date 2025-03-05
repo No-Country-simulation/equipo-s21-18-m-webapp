a02c32020a8c97185530b9c0f516571dcc5442be
Jhuliño A. Oroya Barreto <jaob.4.95@gmail.com>
2025-03-04 23:33:05 -0500

    Feat: Implement exercise details page and loading state
    index.html: Updated the favicon to use the new fitness.svg icon.
    
    fit.svg: Added a new SVG icon for fitness.
    
    fitness.svg: Added a new SVG icon for fitness, which will be used as the favicon.
    
    loading.svg: Added a new SVG loading animation.
    
    ExerciseCard.jsx: Added props to dynamically display exercise information. Also added an event handler for the "Add to Routine" button. Clicking on the card navigates to the exercise detail page.
    
    LoadingView.jsx: Created a loading view component to display while fetching data.
    
    useFetch.js: Created a custom hook for fetching data and handling loading and error states. Implemented simple caching for fetched data.
    
    ExerciseDetail.jsx: Implemented dynamic routing and data fetching for exercise details. The page now displays detailed exercise information retrieved from the API. Integrated loading and error handling. Translated some text to Spanish.
    
    ExerciseLibrary.jsx: Integrated API data fetching and loading state into the exercise library page. The page now displays exercises from the backend and a loading state while fetching data. Translated 'Exercise Library' to Spanish as 'Biblioteca de Ejercicios'.
    
    Home.jsx: Improved code formatting and responsiveness. Added a cursor pointer to buttons.

--- frontend/src/components/ExerciseCard.jsx
+++ frontend/src/components/ExerciseCard.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
 
-export const ExerciseCard = () => {
+export const ExerciseCard = ({ exercise }) => {
+  const onRoutinesBtn = (event) => {
+    event.stopPropagation(); // Evita la navegación
+    event.preventDefault(); // Evita el comportamiento predeterminado del Link
+    console.log("routines");
+  };
   return (
-    <Link to={"/exercise-detail/1"}>
+    <Link to={`/exercise-detail/${exercise._id}`}>
       <div className="rounded-lg border border-gray-200 p-6 transition-shadow hover:shadow-lg">
-        <h2 className="mb-2 text-2xl font-semibold">Push-ups</h2>
+        <h2 className="mb-2 text-2xl font-semibold">{exercise.tittle}</h2>
         <p className="mb-4 font-medium text-gray-500">
-          Strength Training | Beginner
+          {exercise.type} | {exercise.level}
         </p>
-        <button className="inline-flex h-10 items-center justify-center gap-2 rounded-md bg-[#b50d50] px-4 py-2 text-sm font-medium whitespace-nowrap text-white transition-colors hover:bg-[#e51065]">
-          View Details
-        </button>
+        <div className="flex space-x-2">
+          <button className="inline-flex h-10 items-center justify-center gap-2 rounded-md border border-gray-200 px-4 py-2 text-sm font-medium whitespace-nowrap transition-colors hover:bg-gray-100">
+            Ver más
+          </button>
+          <button
+            className="inline-flex h-10 items-center justify-center gap-2 rounded-md bg-[#b50d50] px-4 py-2 text-sm font-medium whitespace-nowrap text-white transition-colors hover:bg-[#e51065]"
+            onClick={onRoutinesBtn}
+          >
+            Añadir a la rutina
+          </button>
+        </div>
       </div>
     </Link>
   );
--- frontend/src/pages/ExerciseLibrary.jsx
+++ frontend/src/pages/ExerciseLibrary.jsx
@@ -1,37 +1,50 @@
 import { ExerciseCard } from "../components/ExerciseCard";
+import { LoadingView } from "../components/LoadingView";
+import { useFetch } from "../hooks/useFetch";
 
 export const ExerciseLibrary = () => {
+  const { data, hasError, isLoading } = useFetch(
+    "https://equipo-s21-18-m-webapp.onrender.com/exercises",
+  );
   return (
     <>
-      <div className="container mx-auto py-10">
-        <h1 className="mb-8 text-4xl font-bold">Exercise Library</h1>
-        <div className="mb-8 flex flex-col gap-4 md:flex-row">
-          <input
-            className="flex h-10 w-full rounded-md border border-gray-200 px-3 py-2 text-sm placeholder:text-gray-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:w-1/3"
-            type="text"
-            placeholder="Search exercises..."
-          />
+      {isLoading ? (
+        <LoadingView />
+      ) : (
+        <div className="container mx-auto py-10">
+          <h1 className="mb-8 text-4xl font-bold">Biblioteca de Ejercicios</h1>
+          <div className="mb-8 flex flex-col gap-4 md:flex-row">
+            <input
+              className="flex h-10 w-full rounded-md border border-gray-200 px-3 py-2 text-sm placeholder:text-gray-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:w-1/3"
+              type="text"
+              placeholder="Search exercises..."
+            />
 
-          <select className="rounded-md border border-gray-200 p-2 tracking-wider focus:border focus:border-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none md:w-1/3">
-            <option value="">All Categories</option>
-            <option value="Strength Training">Strength Training</option>
-            <option value="Cardio">Cardio</option>
-            <option value="Flexibility">Flexibility</option>
-            <option value="HIIT">HIIT</option>
-            <option value="Yoga">Yoga</option>
-            <option value="Core">Core</option>
-          </select>
-          <select className="rounded-md border border-gray-200 p-2 tracking-wider focus:border focus:border-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none md:w-1/3">
-            <option value="">All Difficulties</option>
-            <option value="Beginner">Beginner</option>
-            <option value="Intermediate">Intermediate</option>
-            <option value="Advanced">Advanced</option>
-          </select>
+            <select className="rounded-md border border-gray-200 p-2 tracking-wider focus:border focus:border-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none md:w-1/3">
+              <option value="">All Categories</option>
+              <option value="Strength Training">Strength Training</option>
+              <option value="Cardio">Cardio</option>
+              <option value="Flexibility">Flexibility</option>
+              <option value="HIIT">HIIT</option>
+              <option value="Yoga">Yoga</option>
+              <option value="Core">Core</option>
+            </select>
+            <select className="rounded-md border border-gray-200 p-2 tracking-wider focus:border focus:border-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none md:w-1/3">
+              <option value="">All Difficulties</option>
+              <option value="Beginner">Beginner</option>
+              <option value="Intermediate">Intermediate</option>
+              <option value="Advanced">Advanced</option>
+            </select>
+          </div>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {data
+              ? data.map((exercise) => (
+                  <ExerciseCard key={exercise._id} exercise={exercise} />
+                ))
+              : "Datos no disponibles"}
+          </div>
         </div>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-          <ExerciseCard />
-        </div>
-      </div>
+      )}
     </>
   );
 };
